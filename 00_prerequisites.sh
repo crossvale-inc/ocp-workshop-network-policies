05330d0c57c2cc0aebbd5915d16e86e9f9e621c2
Chad J. Hamilton <chamilton@crossvale.com>
2024-03-28 14:09:57 -0400

    Updated for Prefix Namespaces

--- 00_prerequisites.sh
+++ 00_prerequisites.sh
@@ -4,10 +4,27 @@
 export PREFIX=
 echo "Enter Username"
 read PREFIX
+export PREFIX=${PREFIX}
 
 # Create Environment Prefix Variables if applicable
-# simpson
-# bouvier
-# burns
-# network-policy-demo-monitor
+if [ ! "${PREFIX}" == "" ] ; then
+    # simpson
+    export SIMPSONPREFIX=${PREFIX}-simpson
+    # bouvier
+    export BOUVIERPREFIX=${PREFIX}-bouvier
+    # burns
+    export BURNSPREFIX=${PREFIX}-burns
+    # network-policy-demo-monitor
+    export MONITORPREFIX=${PREFIX}-network-policy-demo-monitor
+else
+    # simpson
+    export SIMPSONPREFIX=simpson
+    # bouvier
+    export BOUVIERPREFIX=bouvier
+    # burns
+    export BURNSPREFIX=burns
+    # network-policy-demo-monitor
+    export MONITORPREFIX=network-policy-demo-monitor
+fi
+
 

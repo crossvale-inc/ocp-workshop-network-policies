0bc6a7282e0459e8ad3f95c6da7665dc5cd4cab8
Chad J. Hamilton <chamilton@crossvale.com>
2024-03-28 11:27:28 -0400

    Updated for project cleanup with prefixes

--- 08_cleanup-deployments.sh
+++ 08_cleanup-deployments.sh
@@ -1,57 +1,68 @@
 #!/bin/bash
 
-# oc projects | grep ${PREFIX}simpson | awk -F" " '{print $1}'
-
 # Start at the Default Namespace
 oc project default
 
 # Cleanup Network Policy Demo Monitor
-REMOVEMONITOR=$(oc projects | grep ${MONITORPREFIX:-'network-policy-demo-monitor'} | awk -F" " '{print $1}')
-if [ "!${REMOVEMONITOR}" == "" ] ; then
+export MONITORPREFIX=${MONITORPREFIX:-'network-policy-demo-monitor'}
+export REMOVEMONITOR=$(oc projects | grep ${MONITORPREFIX} | awk -F" " '{print $1}')
+
+if [ ! "${REMOVEMONITOR}" == "" ] ; then
     oc project ${REMOVEMONITOR}
     oc delete all --all
+    sleep 1
 fi
 
 # Cleanup Simpson
-REMOVESIMPSON=$(oc projects | grep ${SIMPSONPREFIX:-'simpson'} | awk -F" " '{print $1}')
-if [ "!${REMOVESIMPSON}" == "" ] ; then
+export SIMPSONPREFIX=${SIMPSONPREFIX:-'simpson'}
+export REMOVESIMPSON=$(oc projects | grep ${SIMPSONPREFIX} | awk -F" " '{print $1}')
+if [ ! "${REMOVESIMPSON}" == "" ] ; then
     oc project ${REMOVESIMPSON}
     oc delete all --all
+    sleep 1
 fi
 
 # Cleanup Bouvier
-REMOVEBOUVIER=$(oc projects | grep ${BOUVIERPREFIX:-'bouvier'} | awk -F" " '{print $1}')
-if [ "!${REMOVEBOUVIER}" == "" ] ; then
+export BOUVIERPREFIX=${BOUVIERPREFIX:-'bouvier'}
+export REMOVEBOUVIER=$(oc projects | grep ${BOUVIERPREFIX} | awk -F" " '{print $1}')
+if [ ! "${REMOVEBOUVIER}" == "" ] ; then
     oc project ${REMOVEBOUVIER}
     oc delete all --all
+    sleep 1
 fi
 
 # Cleanup Burns
-REMOVEBURNS=$(oc projects | grep ${BURNSPREFIX:-'bouvier'} | awk -F" " '{print $1}')
-if [ "!${REMOVEBURNS}" == "" ] ; then
+export BURNSPREFIX=${BURNSPREFIX:-'burns'}
+export REMOVEBURNS=$(oc projects | grep ${BURNSPREFIX} | awk -F" " '{print $1}')
+if [ ! "${REMOVEBURNS}" == "" ] ; then
     oc project ${REMOVEBURNS}
     oc delete all --all
+    sleep 1
 fi
 
 # Cleanup Projects
 oc project default
 
 # Remove Network Policy Demo Monitor
-if [ "!${REMOVEMONITOR}" == "" ] ; then
+if [ ! "${REMOVEMONITOR}" == "" ] ; then
     oc delete project ${REMOVEMONITOR}
+    sleep 1
 fi
 
 # Remove Simpson
-if [ "!${REMOVESIMPSON}" == "" ] ; then
+if [ ! "${REMOVESIMPSON}" == "" ] ; then
     oc delete project ${REMOVESIMPSON}
+    sleep 1
 fi
 
 # Remove Bouvier
-if [ "!${REMOVEBOUVIER}" == "" ] ; then
+if [ ! "${REMOVEBOUVIER}" == "" ] ; then
     oc delete project ${REMOVEBOUVIER}
+    sleep 1
 fi
 
 # Remove Burns
-if [ "!${REMOVEBURNS}" == "" ] ; then
+if [ ! "${REMOVEBURNS}" == "" ] ; then
     oc delete project ${REMOVEBURNS}
+    sleep 1
 fi
